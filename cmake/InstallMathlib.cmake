b4514df383984d5c0b80c93edf6ad6c520474f95
azubov <azubovv@yandex.ru>
2025-12-29 17:29:55 +0000

    install config

--- cmake/InstallMathlib.cmake
+++ cmake/InstallMathlib.cmake
@@ -5,35 +5,35 @@ if(NOT TARGET mathlib)
     message(FATAL_ERROR "Target mathlib must be defined before including InstallMathlib.cmake")
 endif()
 
-# Установить заголовки (копируем папку include)
+# install headers
 install(DIRECTORY "${CMAKE_SOURCE_DIR}/include/" DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
-# Установить таргет и экспортировать его с namespace mathlib::
+# install target and export
 install(TARGETS mathlib
-        EXPORT MathlibTargets
+        EXPORT mathlibTargets
         ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
         INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
-install(EXPORT MathlibTargets
-        FILE MathlibTargets.cmake
+install(EXPORT mathlibTargets
+        FILE mathlibTargets.cmake
         NAMESPACE mathlib::
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/Mathlib)
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/mathlib)
 
-# Сгенерировать и установить MathlibConfig.cmake и MathlibConfigVersion.cmake
-set(CONFIG_INSTALL_DIR ${CMAKE_INSTALL_LIBDIR}/cmake/Mathlib)
+# generate config files
+set(CONFIG_INSTALL_DIR ${CMAKE_INSTALL_LIBDIR}/cmake/mathlib)
 configure_package_config_file(
         "${CMAKE_CURRENT_LIST_DIR}/MathlibConfig.cmake.in"
-        "${CMAKE_CURRENT_BINARY_DIR}/MathlibConfig.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/mathlibConfig.cmake"
         INSTALL_DESTINATION ${CONFIG_INSTALL_DIR})
 
 write_basic_package_version_file(
-        "${CMAKE_CURRENT_BINARY_DIR}/MathlibConfigVersion.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/mathlibConfigVersion.cmake"
         VERSION ${PROJECT_VERSION}
         COMPATIBILITY AnyNewerVersion)
 
 install(FILES
-        "${CMAKE_CURRENT_BINARY_DIR}/MathlibConfig.cmake"
-        "${CMAKE_CURRENT_BINARY_DIR}/MathlibConfigVersion.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/mathlibConfig.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/mathlibConfigVersion.cmake"
         DESTINATION ${CONFIG_INSTALL_DIR})
